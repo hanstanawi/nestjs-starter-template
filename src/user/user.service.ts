b5f51875e22fb73ccc55cda35af5f295365b1926
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-15 15:05:16 +0800

    build: add user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+import { CreateUserDto, UpdateUserDto } from './dtos';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  public findUsers(limit: number = 10): Promise<User[]> {
+    return this.prismaService.user.findMany({
+      take: limit,
+      skip: 1,
+    });
+  }
+
+  public findUserById(id: string): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  public insertUser(dto: CreateUserDto) {}
+
+  public updateUser(id: string, dto: UpdateUserDto) {}
+
+  public deleteUser(id: string) {
+    return this.prismaService.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}

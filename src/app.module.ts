a57ae8dc960372c997350120c9383078c7b2246d
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-21 21:41:28 +0800

    feat: setup pino logger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +8,42 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        autoLogging: process.env.NODE_ENV === 'development',
+        customProps: () => ({
+          context: 'HTTP',
+        }),
+        customLogLevel: (req, res, err) => {
+          if (res.statusCode >= 400 || err) {
+            return 'error';
+          } else if (res.statusCode >= 300 && res.statusCode < 400) {
+            return 'silent';
+          }
+          return 'info';
+        },
+        serializers: {
+          req(req) {
+            req.body = req.raw.body;
+            return req;
+          },
+        },
+        customErrorMessage: (req, res) => {
+          return 'Request errored with status code: ' + res.statusCode;
+        },
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 
 const DEFAULT_PORT = 3333;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
